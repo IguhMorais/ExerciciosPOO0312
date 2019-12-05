19dbe3a7dd3e502ac3900e5b0ad0b5625bc44f76
Igor Morais <igorcarvelio.silva@gmail.com>
2019-12-05 10:24:24 -0300

    Conclusao do exercicio 5

--- src/Exercicio05/Operario.java
+++ src/Exercicio05/Operario.java
@@ -1,6 +1,37 @@
 package Exercicio05;
 import Exercicio03.Empregado;
 
-public class Operario{
-    
+public class Operario extends Empregado{
+    private double valorProducao;
+    private double comisao;
+
+    public double getValorProducao(){
+        return this.valorProducao;
+    }
+
+    public void setValorProducao(double vp){
+        this.valorProducao = vp;
+    }
+
+    public double getComisao(){
+        return this.comisao;
+    }
+
+    public void setComisao(double c){
+        this.comisao = c;
+    }
+
+    @Override
+    public double calcularSalario(){
+        return getSalario()+(getValorProducao()*(getComisao()/100));
+    }
+
+    @Override
+    public double calcularSalario(double s,double vp, double c){
+        return s+(vp*(c/100));
+    }
+
+    public Operario(){
+        
+    }
 }

2e003be3eed4ed5f5f0fdbaf12d80994804a9687
Igor Morais <igorcarvelio.silva@gmail.com>
2019-12-05 23:12:50 -0300

    observaçoes a classe de conexao

--- src/Exercicio07/connection/Conector.java
+++ src/Exercicio07/connection/Conector.java
@@ -6,19 +6,19 @@ public class Conector {
 	
 	public static Connection connection = null;
 	private final static String DRIVER = "com.mysql.jdbc.Driver";
-	private final static String DBNAME = "bdExercicio";
-	private final static String URL = "jdbc:mysql://localhost:8080/"+DBNAME;
-	private final static String LOGIN = "root";
-	private final static String SENHA = "";
+	private final static String DBNAME = "bdExercicio";//adicionar nome do banco correto
+	private final static String URL = "jdbc:mysql://localhost:8080/"+DBNAME; //adicionar url correta
+	private final static String LOGIN = "root"; //nome do user do banco
+	private final static String SENHA = "";//senha se houver uma
 	
 	public static Connection getConnection() {
 		try {
 			Class.forName(DRIVER);
 			connection = DriverManager.getConnection(URL,LOGIN,SENHA);
-			System.out.println("Conexão realizada com sucesso");
+			System.out.println("Conexao realizada com sucesso");
 			return connection;
 		}catch (ClassNotFoundException e) {
-			System.out.println("Driver não encontrado "+e.toString());
+			System.out.println("Driver nao encontrado "+e.toString());
 			return null;
 		}catch (Exception e) {
 			System.out.println(e.getMessage());
